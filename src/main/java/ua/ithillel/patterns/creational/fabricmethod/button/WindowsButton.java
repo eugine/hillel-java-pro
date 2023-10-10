2d5cf60f9cda7cbd108f10f1aa12c4a02ce77d41
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-10-10 21:27:51 +0300

    Patterns (#31)
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>

---
+++ src/main/java/ua/ithillel/patterns/creational/fabricmethod/button/WindowsButton.java
@@ -0,0 +1,13 @@
+package ua.ithillel.patterns.creational.fabricmethod.button;
+
+public class WindowsButton implements Button {
+    @Override
+    public void render(int x, int y) {
+        System.out.println("render windows button");
+    }
+
+    @Override
+    public void onClick(String event) {
+        System.out.println("onClick windows event: " + event);
+    }
+}
