ed0b1470160b3b54d6602dee445ad60e891c1214
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-10-16 10:09:41 +0300

    Sockets (#32)
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>

---
+++ src/main/java/ua/ithillel/utils/TestImpl.java
@@ -0,0 +1,15 @@
+package ua.ithillel.utils;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class TestImpl implements Test {
+    static void hello() {
+        System.out.println("hello in impl");
+    }
+
+    public void test() {
+        System.out.println("test in impl");
+    }
+
+}
