f6c4fbd90a79c8af022f2743c5798710f827ef04
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2023-12-04 20:43:38 +0200

    Wiremock

--- src/test/java/ua/ithillel/bank/BaseInMemoryIntegrationTest.java
+++ src/test/java/ua/ithillel/bank/BaseInMemoryIntegrationTest.java
@@ -1,13 +1,19 @@
 package ua.ithillel.bank;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.ithillel.bank.config.WiremockConfig;
 import ua.ithillel.bank.person.PersonRepository;
 
 @SpringBootTest
@@ -20,5 +26,17 @@ public abstract class BaseInMemoryIntegrationTest {
     protected PersonRepository personRepository;
     @Autowired
     protected ObjectMapper objectMapper;
+    @Autowired
+    protected WireMockServer wireMockServer;
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.resetAll();
+    }
+
+    @DynamicPropertySource
+    public static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("wiremock.baseurl", WiremockConfig.wireMockServer::baseUrl);
+    }
 
 }
